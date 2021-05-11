abc8c9e0ac98e0bacb115de36b58548d81d40543
coderj001 <amirajubolchi001@gmail.com>
2021-05-11 11:07:41 +0530

    setup the leads model

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -17,6 +17,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
+    'debug_toolbar',
+
+
+    'leads.apps.LeadsConfig'
 ]
 
 MIDDLEWARE = [
@@ -27,6 +33,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+
 ]
 
 ROOT_URLCONF = 'djcrm.urls'
@@ -85,3 +93,28 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.history.HistoryPanel',
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+    '0.0.0.0',
+    'localhost',
+]
+
+AUTH_USER_MODEL = 'leads.User'
--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,59 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Lead(models.Model):
+    SOURCE_CHOICES = (
+        ('Youtube', 'Youtube'),
+        ('Google', 'Google'),
+        ('NewsLetter', 'NewsLetter'),
+        ('Others', 'Others'),
+    )
+    first_name = models.CharField(
+        max_length=25,
+        verbose_name='first name',
+        null=True
+    )
+    last_name = models.CharField(
+        max_length=25,
+        verbose_name='last name',
+        null=True
+    )
+    age = models.IntegerField(default=0)
+    phoned = models.BooleanField(default=False)
+    source = models.CharField(
+        max_length=50,
+        choices=SOURCE_CHOICES,
+        default="Youtube"
+    )
+    # profile_pic = models.ImageField(blank=True, null=True)
+    # special_files = models.FileField(blank=True, null=True)
+
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Lead"
+        verbose_name_plural = "Leads"
+
+    def __str__(self):
+        return super(Lead, self).__str__()
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(
+        max_length=15,
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = "Agent"
+        verbose_name_plural = "Agents"
+
+    def __str__(self):
+        return super(Agent, self).__str__()
